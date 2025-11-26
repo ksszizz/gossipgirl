3f26df9ecc478ba0380dfde7b9726782623960db
ksszizz <kisslujza2005@gmail.com>
2025-11-26 14:22:16 +0100

    Add files via upload

---
+++ BEADANDO/js/gossip.js
@@ -0,0 +1,15 @@
+﻿const alertContainer = document.getElementById('fullscreen-alert');
+
+function showAlert() {
+    alertContainer.classList.remove('alert-hidden');
+    alertContainer.classList.add('alert-visible');
+    document.body.style.overflow = 'hidden';
+}
+
+function hideAlert() {
+    alertContainer.classList.remove('alert-visible');
+    alertContainer.classList.add('alert-hidden');
+    document.body.style.overflow = 'auto';
+}
+
+window.onload = showAlert;
